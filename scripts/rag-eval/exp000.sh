f98d2e006863a19c1fb6c937e0df65bfb867a2ea
zaemon1251-hesty <hisakawa1251@gmail.com>
2025-02-08 10:05:05 +0900

    add comments

--- scripts/rag-eval/exp000.sh
+++ scripts/rag-eval/exp000.sh
@@ -1,8 +1,11 @@
+# 公開重み(SoccerNet-Tracking Datasetで学習された重み)で選手同定を実行する
+EXP_NAME=$(date '+%Y-%m-%d_%H-%M')
+
+TMP_VIDEO_PATH=/local/moriy/SoccerNetGS/rag-eval
+
 export CUDA_VISIBLE_DEVICES=2; python -m tracklab.main -cn soccernet-v2-exp005 \
     modules.reid.training_enabled=False \
     modules.team._target_=sn_gamestate.team.TrackletTeamClustering \
-    wandb.name=exp000 \
-    dataset.dataset_path="/local/moriy/SoccerNetGS/rag-eval" \
-    'dataset.vids_dict.test=["SNGS-0029","SNGS-0030","SNGS-0031","SNGS-0032","SNGS-0033"]'
-
+    wandb.name=$EXP_NAME \
+    dataset.dataset_path="$TMP_VIDEO_PATH"
 
--- scripts/rag-eval/exp004.sh
+++ scripts/rag-eval/exp004.sh
@@ -1,9 +1,12 @@
 # 学習済み重みで学習
+# imageに書き出さずに直接videoを入力として処理する
+
+# できなさそう 関連 issue: https://github.com/TrackingLaboratory/tracklab/issues/17
 trained_weights="/local/moriy/model/soccernet/sn-gamestate/reid/sn-gamestate-v3-720p/2024-11-26-10-21-36/0/2024_11_26_10_22_05_22S9c0e99da-e916-4634-95c1-4ab4e94998e5model/job-0_29_model.pth.tar"
 export CUDA_VISIBLE_DEVICES=3; python -m tracklab.main -cn soccernet-v2-exp005 \
     modules.reid.training_enabled=False \
     modules.team._target_=sn_gamestate.team.TrackletTeamClustering \
-    wandb.name=exp001 \
+    wandb.name=video-exp001 \
     modules.reid.cfg.model.load_weights=$trained_weights \
-    dataset.dataset_path="/local/moriy/SoccerNetGS/rag-eval" \
-    'dataset.vids_dict.test=["SNGS-0029","SNGS-0030","SNGS-0031","SNGS-0032","SNGS-0033"]'
+    dataset.dataset_path="/local/moriy/SoccerNetGS/rag-eval/video" \
+    engine=video
